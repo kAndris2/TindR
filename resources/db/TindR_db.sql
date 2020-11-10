6041f312deafcf66d00841f70fba63a9920339b7
kAndris2 <k.andris258@gmail.com>
2020-11-10 12:59:42 +0100

    db--

--- resources/db/TindR_db.sql
+++ resources/db/TindR_db.sql
@@ -1,22 +1,22 @@
 CREATE TABLE accounts (
 	id serial not null PRIMARY KEY,
-	email varchar(30) not null,
-	password varchar(256) not null,
-	phone_number varchar(20)
+	email character varying(30) not null,
+	password character varying(256) not null,
+	phone_number character varying(20)
 );
 
 CREATE TABLE users (
 	account_id int not null PRIMARY KEY,
-	name varchar(20) not null,
+	name character varying(20) not null,
 	age int not null,
-	passion varchar(150) not null,
-	gender varchar(30),
-	description varchar(500),
-	job_title varchar(20),
-	company varchar(20),
-	school varchar(50),
-	anthem varchar(250),
-	sexual_orientation varchar(30),
+	passion character varying(150) not null,
+	gender character varying(30),
+	description character varying(500),
+	job_title character varying(20),
+	company character varying(20),
+	school character varying(50),
+	anthem character varying(250),
+	sexual_orientation character varying(30),
 	global boolean,
 	status boolean,
 	FOREIGN KEY(account_id) REFERENCES accounts(id) ON DELETE CASCADE
@@ -25,7 +25,7 @@ CREATE TABLE users (
 CREATE TABLE searches (
 	account_id int not null PRIMARY KEY,
 	max_distance int CHECK (max_distance <= 161),
-	looking_for varchar(15),
+	looking_for character varying(15),
 	min_age int CHECK(min_age >= 18),
 	max_age int CHECK((max_age != min_age) AND (max_age > min_age)),
 	FOREIGN KEY(account_id) REFERENCES accounts(id) ON DELETE CASCADE
@@ -34,7 +34,7 @@ CREATE TABLE searches (
 CREATE TABLE public.pictures (
 	account_id int not null PRIMARY KEY,
 	upload_date bigint not null,
-	route varchar(300) not null,
+	route character varying(300) not null,
 	FOREIGN KEY(account_id) REFERENCES accounts(id) ON DELETE CASCADE
 );
 
