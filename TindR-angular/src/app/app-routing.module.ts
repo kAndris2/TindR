bd307609c5c2deefe0d494b70c14c07c1d62e1be
kAndris2 <k.andris258@gmail.com>
2020-11-18 14:15:42 +0100

    use component

---
+++ TindR-angular/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+
+import { RegistrationComponent } from "./registration/registration.component";
+
+const routes: Routes = [
+  {path: "registration", component: RegistrationComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [
+  RegistrationComponent
+];
--- TindR-angular/src/app/app.module.ts
+++ TindR-angular/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    routingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
