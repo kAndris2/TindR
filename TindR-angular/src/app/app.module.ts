a8149ea4d25847295a0c8788fda3c823ad0e27df
burgonyapure <lara@laravelTindr.mradmin.hu>
2020-11-24 12:18:46 +0000

    login post

--- TindR-angular/src/app/app.module.ts
+++ TindR-angular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routingComponents } from "./app-routing.module";
@@ -10,6 +12,9 @@ import { AppRoutingModule, routingComponents } from "./app-routing.module";
     routingComponents
   ],
   imports: [
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- TindR-angular/src/app/login/login.component.ts
+++ TindR-angular/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    public fb: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.form = this.fb.group({
+      email: [''],
+      password: ['']
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  submitForm() {
+    var formData: any = new FormData();
+    formData.append("email", this.form.get('email').value);
+    formData.append("password", this.form.get('password').value);
+    console.log(this.form.value);
+    // this.http.post('http://localhost:4000/api/create-user', formData).subscribe(
+    //   (response) => console.log(response),
+    //   (error) => console.log(error)
+    // )
+  }
 }
