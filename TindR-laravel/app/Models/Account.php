9e027a2f5276bd2fa05f7fe429a1de71c8ef7174
kAndris2 <k.andris258@gmail.com>
2020-11-24 15:37:46 +0100

    gfdg

--- TindR-laravel/app/Models/Account.php
+++ TindR-laravel/app/Models/Account.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model {
     public $timestamps = false;
-<<<<<<< HEAD
-=======
-
->>>>>>> e32a0e315e64e0ea1c633c131dae221fa4ad2b6f
     protected $fillable = [
         'email',
         'password',
