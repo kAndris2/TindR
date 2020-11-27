c3128003ad5931253d4d509c036a140407552b0f
kAndris2 <k.andris258@gmail.com>
2020-11-27 13:38:42 +0100

    handle default route

--- tindr-react/src/App.js
+++ tindr-react/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Helmet } from 'react-helmet';
+
+import WelcomePage from "./components/WelcomePage";
 
 class App extends Component {
   constructor() {
@@ -15,38 +16,15 @@ class App extends Component {
   render() {
     return (
       <>
-        <section style={{ height:"100vh",
-                          background: 'linear-gradient(rgba(0,0,0,.4), rgba(0,0,0,.4)), url("https://tinder.com/static/build/m/b07fe760ab4714aa3e932a15657494e5.webp")',
-                          backgroundRepeat:"no-repeat"}}
-        >
-          <nav className="navbar navbar-light bg-transparent justify-content-between">
-            <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="/">
-                  <span className="sr-only">TindR</span>
-                  <img src="logo.svg" width="55%"></img>
-              </a>
-            </div>   
+        <Router>
+          <Switch>
 
-            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a href="/login" 
-                  className="btn btn-success rounded-pill">
-                  LOG IN
-              </a>
-            </div>
-          </nav>
+            <Route exact path="/">
+              <WelcomePage></WelcomePage>
+            </Route>
 
-          <div className="container d-flex h-100 align-items-center">
-            <div className="mx-auto text-center">
-              <h1 className="mx-auto my-0" style={{fontSize:"800%"}}>
-                Swipe Right®
-              </h1>
-              <a className="btn btn-danger js-scroll-trigger rounded-pill" href="/">
-                CREATE ACCOUNT 
-              </a>
-            </div>
-          </div>
-
-        </section>
+          </Switch>
+        </Router>
       </>
     );
   }
---
+++ tindr-react/src/components/WelcomePage.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import { Helmet } from 'react-helmet';
+
+class WelcomePage extends Component {
+    constructor() {
+      super();
+    }
+  
+    render() {
+      return (
+        <>
+          <Helmet>
+              <title>Tinder | Dating, Make Friends & Meet New People</title>
+          </Helmet>
+  
+          <section style={{ height:"100vh",
+                            background: 'linear-gradient(rgba(0,0,0,.4), rgba(0,0,0,.4)), url("https://tinder.com/static/build/m/b07fe760ab4714aa3e932a15657494e5.webp")',
+                            backgroundRepeat:"no-repeat"}}
+          >
+            <nav className="navbar navbar-light bg-transparent justify-content-between">
+              <div className="flex justify-start lg:w-0 lg:flex-1">
+                <a href="/">
+                    <span className="sr-only">TindR</span>
+                    <img src="logo.svg" width="55%"></img>
+                </a>
+              </div>   
+  
+              <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                <a href="/login" 
+                    className="btn btn-success rounded-pill">
+                    LOG IN
+                </a>
+              </div>
+            </nav>
+  
+            <div className="container d-flex h-100 align-items-center">
+              <div className="mx-auto text-center">
+                <h1 className="mx-auto my-0" style={{fontSize:"800%"}}>
+                  Swipe Right®
+                </h1>
+                <a className="btn btn-danger js-scroll-trigger rounded-pill" href="/registration">
+                  CREATE ACCOUNT 
+                </a>
+              </div>
+            </div>
+  
+          </section>
+        </>
+      );
+    }
+  }
+  
+  export default WelcomePage;
