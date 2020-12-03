1b90d061e12a7080e777a79928077bc469680d08
kAndris2 <k.andris258@gmail.com>
2020-12-03 13:39:27 +0100

    getnext

--- tindr-react/src/components/Recommendations.js
+++ tindr-react/src/components/Recommendations.js
@@ -8,10 +8,15 @@ class Recommendations extends Component {
 
       this.state = {
           recommendations: [],
-          isLoading: true
+          isLoading: true,
+          current: {
+              index: undefined,
+              user: undefined
+          }
       }
 
       this.getRecommendations = this.getRecommendations.bind(this);
+      this.getNext = this.getNext.bind(this);
     }
 
     async componentDidMount() {
@@ -21,23 +26,45 @@ class Recommendations extends Component {
     async getRecommendations() {
         await axios.get("http://localhost:8000/api/recommendations/" + this.props.userID)
         .then(response => {
+            let updCurrent = {
+                index: 0,
+                user: response.data[0]
+            }
+
             this.setState({
                 isLoading : false,
-                recommendations : response.data
+                recommendations : response.data,
+                current : updCurrent
             });
         })
     }
 
+    getNext() {
+        const { recommendations, current } = this.state;
+
+        if (recommendations[current.index + 1] !== undefined) {
+            let updCurrent = {
+                index: current.index + 1,
+                user: recommendations[current.index + 1]
+            }
+            this.setState({current : updCurrent})
+        }
+    }
+
     render() {
-        const { isLoading, recommendations } = this.state;
+        const { isLoading, current } = this.state;
 
         if (!isLoading) {
-            console.log(recommendations)
             return (
                 <>
-                    {recommendations.map(rec =>
-                        <p>{rec.name}</p>
-                    )}
+                    <div className="container d-flex h-100 align-items-center">
+                        <div className="mx-auto text-center">
+                            <h1 className="mx-auto my-0 text-red" style={{fontSize:"800%"}}>
+                            {current.user.name}
+                            </h1>
+                            <button onClick={this.getNext}>Next</button>
+                        </div>
+                    </div>
                 </>
             );
         }
