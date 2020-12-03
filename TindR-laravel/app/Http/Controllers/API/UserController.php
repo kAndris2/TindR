cf6277ef48ac43ad5a62e860258b43921a2d2755
kAndris2 <k.andris258@gmail.com>
2020-12-03 12:57:12 +0100

    fdsf

--- TindR-laravel/app/Http/Controllers/API/UserController.php
+++ TindR-laravel/app/Http/Controllers/API/UserController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Search;
+use App\Models\Match;
+use App\Models\Like;
+use App\Models\Dislike;
 
 class UserController extends Controller
 {
@@ -21,5 +25,18 @@ class UserController extends Controller
     public function updateUser(Request $request, $id)
     {
         User::find($id)->update($request->all());
+    }
+
+    public function getRecommendations($id)
+    {
+        //$recommendations = array();
+        $users = User::all();
+        /*
+        $searches = Search::all();
+        $matches = Match::all();
+        $likes = Like::all();
+        $dislikes = Dislike::all();
+        */
+        return $users;
     }
 }
