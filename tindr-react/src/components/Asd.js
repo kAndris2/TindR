a0ef02c607f08c7daad09d090dc072b02504a478
burgonyapure <lara@laravelTindr.mradmin.hu>
2020-12-09 10:01:20 +0000

    pull

--- tindr-react/src/components/Asd.js
+++ tindr-react/src/components/Asd.js
@@ -3,15 +3,21 @@ import SideBar from './SideBar'
 import './css/side.css'
 
 export default class Asd extends Component {
-    render() {
-        return (
-          <div className="App" id="outer-container">
-            <SideBar removeCookie={this.props.removeCookie} user={this.props.user} pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-          <div id="page-wrap">
-            <h1>Huzogass</h1>
-            <h2>Jobbrabalra stb</h2>
-          </div>
+  constructor(props){
+    super(props);
+
+
+  }
+
+  render() {
+      return (
+        <div className="App" id="outer-container">
+          <SideBar removeCookie={this.props.removeCookie} user={this.props.user} pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+        <div id="page-wrap">
+          <h1>Huzogass</h1>
+          <h2>Jobbrabalra stb</h2>
         </div>
-        );
-    }
+      </div>
+      );
+  }
 }
--- tindr-react/src/components/SideBar.js
+++ tindr-react/src/components/SideBar.js
@@ -5,7 +5,7 @@ export default class SideBar extends Component {
   constructor(props){
     super(props);
   }
-
+  
 
   render() {
     return (
