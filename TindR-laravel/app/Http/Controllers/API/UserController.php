24bb411d346fceea1246ea7d6c583e32d0ba48fc
kAndris2 <k.andris258@gmail.com>
2020-12-14 12:24:28 +0100

    Profile model

--- TindR-laravel/app/Http/Controllers/API/UserController.php
+++ TindR-laravel/app/Http/Controllers/API/UserController.php
@@ -10,6 +10,9 @@ use App\Models\Match;
 use App\Models\Like;
 use App\Models\Dislike;
 
+use App\Models\Profile;
+use App\Models\Picture;
+
 class UserController extends Controller
 {
     public function getUsers() 
@@ -38,5 +41,25 @@ class UserController extends Controller
         $dislikes = Dislike::all();
         */
         return $users;
+    }
+
+    public function getProfile($id)
+    {
+        $user = User::find($id);
+        $imgs = Picture::where("user_id", "=", $id);
+        $img_arr = array();
+
+        foreach($imgs as $img)
+        {
+            array_push($img_arr, $img->route);
+        }
+
+        return new Profile(
+            $user->id,
+            $user->name,
+            27,
+            "3 miles away",
+            "bla bla bla..."
+        );
     }
 }
