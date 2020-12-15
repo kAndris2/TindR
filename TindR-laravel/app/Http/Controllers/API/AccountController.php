19dce20292c1398a7b72a5187821b2c0004e9dd3
kAndris2 <k.andris258@gmail.com>
2020-12-15 10:55:12 +0100

    fix

--- TindR-laravel/app/Http/Controllers/API/AccountController.php
+++ TindR-laravel/app/Http/Controllers/API/AccountController.php
@@ -80,7 +80,7 @@ class AccountController extends Controller
 
     public function getDetails($id)
     {
-        $account = Account::find($id)->first();
+        $account = Account::find($id)->get();
 
         return array(
             'email' => $account->email,
