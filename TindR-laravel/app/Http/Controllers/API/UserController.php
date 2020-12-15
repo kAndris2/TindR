3ce56d548f33acbee5fc208a5adce5e3324dec93
kAndris2 <k.andris258@gmail.com>
2020-12-15 10:04:26 +0100

    dd

--- TindR-laravel/app/Http/Controllers/API/UserController.php
+++ TindR-laravel/app/Http/Controllers/API/UserController.php
@@ -10,7 +10,6 @@ use App\Models\Match;
 use App\Models\Like;
 use App\Models\Dislike;
 
-use App\Models\Profile;
 use App\Models\Picture;
 
 class UserController extends Controller
@@ -43,23 +42,48 @@ class UserController extends Controller
         return $users;
     }
 
-    public function getProfile($id)
+    public function getRecom2($id)
     {
-        $user = User::find($id);
-        $imgs = Picture::where("user_id", "=", $id);
-        $img_arr = array();
+        $users = User::where("id", "!=", $id)->get();
+        //$imgs = Picture::where("user_id", "!=", $id)->get();
 
-        foreach($imgs as $img)
+        $recoms = array();
+
+        foreach($users as $user)
         {
-            array_push($img_arr, $img->route);
+            /*
+            $img_arr = array();
+            foreach($imgs as $img)
+            {
+                if ($img->user_id == $user->id)
+                {
+                    array_push($img_arr, $img->route);
+                }
+            }
+            */
+            $temp = array(
+                'id' => $user->id,
+                'name' => $user->name,
+                'age' => 27,
+                'distance' => '3 miles away',
+                'text' => 'bla bla bla...'
+                //'img' => $img_arr
+            );
+            var_dump( json_encode($temp) );
+            //array_push($recoms, json_encode($temp));
         }
 
-        return new Profile(
-            $user->id,
-            $user->name,
-            27,
-            "3 miles away",
-            "bla bla bla..."
+        /*
+        $recoms = array(
+            'id' => $users->id,
+            'name' => $users->name,
+            'age' => 27,
+            'distance' => '3 miles away',
+            'text' => 'bla bla bla...',
+            'img' => $img_arr
         );
+        */
+
+        //return json_encode($recoms);
     }
 }
